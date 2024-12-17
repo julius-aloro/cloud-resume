81850ea09fa1174071a871e78558d7211b9a19ef
Julius Wayne Aloro <juliuswaynealoro@gmail.com>
2024-12-17 16:10:19 +0800

    updated index.html file, added outputs to root module

--- infra/cloudfront/outputs.tf
+++ infra/cloudfront/outputs.tf
@@ -6,3 +6,6 @@ output "cf_hosted_zone" {
   value = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
 }
 
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
